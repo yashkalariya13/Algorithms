464cf7b6574e9274ab072abf4c132c9fa7edbc37
Asmit Kumar <114564653+Asmit-Kumar0@users.noreply.github.com>
2022-10-01 15:15:11 +0530

    hactoberfest-2022

---
+++ binary_conversion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void main()
+{
+    int n,i=0,z,j;
+    scanf("%d",&n);
+    int *a = (int*)malloc(sizeof(int));
+    
+    while(n>0)
+    {
+        z=n%2;
+        a[i]=z;
+        i++;
+        n/=2;
+    }
+    i--;
+    while(i>=0)
+    {
+        printf("%d",a[i]);
+        i--;
+    }
+}
