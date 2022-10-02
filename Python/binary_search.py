a3e0bb338b842c4c4c5a25cd8193eb64ab36174d
Tdms2589 <103559784+Tdms2589@users.noreply.github.com>
2022-10-03 02:31:51 +0530

    binary_search

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,35 +1,43 @@
-def binary_search(arr, low, high, x):
- 
-    # Check base case
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        # If element is present at the middle itself
-        if arr[mid] == x:
-            return mid
- 
-        # If element is smaller than mid, then it can only
-        # be present in left subarray
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
- 
-        # Else the element can only be present in right subarray
-        else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        # Element is not present in the array
-        return -1
- 
-# Test array
-arr = [ 2, 3, 4, 10, 40 ]
-x = 10
- 
-# Function call
-result = binary_search(arr, 0, len(arr)-1, x)
- 
-if result != -1:
-    print("Element is present at index", str(result))
-else:
-    print("Element is not present in array")
+import random
+
+
+l = []
+
+for i in range(32):
+    l.append(random.randint(1,100))
+       
+l.sort()
+print(l)
+k = int(input("enter value of k:")) 
+def binary_search(l,k):
+  
+  b=0
+  end=len(l)-1
+  
+  
+  while(end-b>1):
+    mid=(end+b)//2
+    
+    if(l[mid]==k):
+      print("1")
+      return 1
+    if(l[mid]>k):
+      end=mid-1
+    if(l[mid]<k):
+      b=mid+1
+      
+  if(l[b]==k) or (l[end]==k):
+    print("1")
+    return 1
+  else:
+    print("0")
+    return 0
+    
+binary_search(l,k)
+
+
+  
+        
+
+
+    
