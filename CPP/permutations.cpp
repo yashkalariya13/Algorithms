dca36c6673ee9af8da866bd2c211e209c42060d9
ashish-nhk <89041437+ashish-nhk@users.noreply.github.com>
2022-10-02 18:31:45 +0530

    permutations.cpp

--- CPP/permutations.cpp
+++ CPP/permutations.cpp
@@ -4,12 +4,12 @@ public:
         if(pos == nums.size()){
             ans.push_back(nums);
         }
-    else{
-        for(int i=pos; i<nums.size(); i++){
-            swap(nums[i], nums[pos]);
-            rec(pos+1, nums, ans);
-            swap(nums[i], nums[pos]);
-        }
+        else{
+             for(int i=pos; i<nums.size(); i++){
+                 swap(nums[i], nums[pos]);
+                 rec(pos+1, nums, ans);
+                 swap(nums[i], nums[pos]);
+             }
       }
      }
     vector<vector<int>> permute(vector<int>& nums) {
