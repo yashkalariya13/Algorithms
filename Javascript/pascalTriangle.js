2918165742f012f2e34ad5e00c71d98f9a2ac371
Arie <91861324+ariear@users.noreply.github.com>
2022-10-02 09:21:11 +0700

    add: Pascal's Triangle js

---
+++ Javascript/pascalTriangle.js
@@ -0,0 +1,30 @@
+const generate = (numRows) => {
+    let triangle = [[1], [1,1]]
+    
+    if(numRows === 0){
+        return []
+    }else if(numRows == 1){
+        return [[1]]
+    }else if(numRows == 2){
+        return [[1], [1,1]]
+    }else{
+        for(let i = 2; i < numRows; i++){
+            addRow(triangle)
+        }
+    }
+    return triangle
+}
+
+const addRow = (triangle) => {
+    let previous = triangle[triangle.length - 1]
+    let newRow = [1]
+    for(let i = 0; i < previous.length - 1; i++){
+        let current =  previous[i]
+        let next = previous[i+1]
+        newRow.push(current + next)
+    }
+    newRow.push(1)
+    return triangle.push(newRow)
+}
+
+console.log(generate(10))
