b0955ad624b51ed74bd370ad654f25006ec10328
SharibSaifi <91753197+SharibSaifi@users.noreply.github.com>
2022-10-04 01:55:52 +0530

    Class

---
+++ class.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+// Example 1 of Normal Class -
+/*class Student{
+    public:
+    string name;
+    int id;
+    int age;
+};
+int main(){
+    Student s1,s2,s3;
+    s1.name="Sharib";
+    s1.id=14000;
+    s1.age=21;
+    s2.name="Shivam";
+    s2.id=14001;
+    s2.age=22;
+    s3.name="Shruti";
+    s3.id=14002;
+    s3.age=23;
+    cout<<"Details of student 1 : "<<"\n"<<"Name = "<<s1.name<<"\n"<<"Id = "<<s1.id<<"\n"<<"Age = "<<s1.age<<endl;
+    cout<<"Details of student 2 : "<<"\n"<<"Name = "<<s2.name<<"\n"<<"Id = "<<s2.id<<"\n"<<"Age = "<<s2.age<<endl;
+    cout<<"Details of student 3 : "<<"\n"<<"Name = "<<s3.name<<"\n"<<"Id = "<<s3.id<<"\n"<<"Age = "<<s3.age<<endl;
+    return 0;
+}*/
+
+// Example 2 of normal classes - 
+/*class rectangle{
+    public:
+    int Area(int a,int b){
+        return a*b;
+    }
+};
+int main(){
+    rectangle rec;
+    cout<<rec.Area(10,5)<<endl;
+    return 0;
+}*/
+
+// Example 3 of normal classes -
+/*class Circle{
+    private:
+    int r;
+    public:
+    void setvalues(int);
+    float Area(){
+        return 3.14*r*r;
+    }
+};
+void Circle::setvalues(int x){
+    r=x;
+}
+int main(){
+    Circle crl;
+    crl.setvalues(5);
+    cout<<crl.Area()<<endl;
+    return 0;
+}*/
+
