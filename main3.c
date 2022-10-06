90cc410708a7b326df4fba216abcfe4214075458
bhuwan-2003 <112974223+bhuwan-2003@users.noreply.github.com>
2022-10-06 19:16:41 +0530

    Add files via upload
    Hacktoberfest2022

---
+++ main3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,sum;
+	printf("Enter the value of a: ");
+	scanf("%d",&a);
+	
+	printf("Enter the value of b: ");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	printf("Addition of integer: %d \n",sum);
+	
+	 float x,y,add;
+	printf("Enter the value of x: ");
+	scanf("%f",&x);
+	
+	printf("Enter the value of y: ");
+	scanf("%f",&y);
+	
+	add=x+y;
+	printf("Addition of float: %d \n",add);
+}
